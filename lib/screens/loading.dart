9e7d68bccc91833a79a36b9e58edbb851dc8c082
AlCh440 <albert.chica@estudiantat.upc.edu>
2023-01-24 19:38:32 +0100

    Don't really understand but I think I got it

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,16 +1,26 @@
-// ignore_for_file: unused_import, implementation_imports
-
 import 'package:deliverabl1task_2/screens/main_screen.dart';
 import 'package:deliverabl1task_2/services/users.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:deliverabl1task_2/screens/login_register.dart';
 import 'package:provider/provider.dart';
+import 'package:deliverabl1task_2/external/oauth_lib/src/authorization_code_grant.dart'
+    as auth;
 
 class LoadingPage extends StatelessWidget {
   const LoadingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var token = Uri(path: 'https://oauth.fatsecret.com/connect/token');
+    var endpoint = Uri(path: 'https://oauth.fatsecret.com/resources');
+    String ident = 'ea55506eb43a43c1a1c5a22f3fea8b7f';
+    String secre = 'a6c58822b9ca467782cd058675189781';
+
+    auth.AuthorizationCodeGrant(ident, endpoint, token,
+      secret: secre,
+      basicAuth: true,
+     // getParameters: (contentType, body) {},
+    );
     AuthUser? user = Provider.of<AuthUser?>(context);
     final bool isLoggedIn = user != null;
     return isLoggedIn ? const MainScreen() : const AuthorizationPage();
